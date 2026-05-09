1c9c5fd9153e50b1e7a14b31ead6a606a385de4e
Lcpt7 <1839013348@qq.com>
2026-05-09 15:21:57 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import random
+
+app = Flask(__name__)
+
+# 鸡汤语录库，你以后可以自己随便加
+chicken_soup = [
+    "生活原本沉闷，但跑起来就有风。",
+    "别贪心，你不可能什么都有；别灰心，你不可能什么都没有。",
+    "前路漫漫亦灿灿，往事堪堪亦澜澜。",
+    "坚持的意义，是让自己有选择的权利。",
+    "所有的好运，都藏在温柔和坚持里。",
+    "不必追光，你我皆是星辰。"
+]
+
+# 主页路由
+@app.route('/', methods=["GET", "POST"])
+def index():
+    soup = ""
+    if request.method == "POST":
+        # 不管用户输入啥，都随机给一句鸡汤
+        soup = random.choice(chicken_soup)
+    return render_template("index.html", soup=soup)
+
+if __name__ == "__main__":
+    app.run(debug=True)
